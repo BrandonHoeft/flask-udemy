eec7de4144801798b7278d592ff44a61219aae19
bhoeft <brandonmhoeft@gmail.com>
2022-10-15 18:39:57 -0500

    section 2: Handling GET & POST requests

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # runbook: $ `flask run` from CLI for dev/test
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')  # binds a URL to a function
+@app.route('/')  # binds a URL to a function. By default these are GET requests
 def welcome():
     return "My first Flask app! THIS IS ROOT!"
 
@@ -12,3 +12,9 @@ def welcome():
 def what_is_dunder():
     return f"The value of __name__ is: {__name__}"
 
+@app.route('/method', methods=["GET", "POST"])
+def method():
+    if request.method == "POST":
+        return "You've used a POST request"
+    else:
+        return "You're probably using a GET request, as it's a default!"
