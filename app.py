f4c7df7f9979f784fb74f32d605988f3e0a7c0f8
bhoeft <brandonmhoeft@gmail.com>
2022-10-16 14:35:11 -0500

    bugfix: move return up one indent in app.py. submit -> Submit in index.html

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET","POST"])  # app.route binds a URL to a function
+@app.route('/', methods=["GET", "POST"])  # app.route binds a URL to a function
 def rootpage():
     name = ''
     if request.method == "POST" and "username" in request.form:
         name = request.form.get("username")
-        return render_template("index.html",  # looks in templates/
-                               name=name)  # we're giving name variable to our
+    return render_template("index.html",  # looks in templates/
+                           name=name)  # we're giving name variable to our
 
-@app.route('/name')  # by default, this is a GET request
-def what_is_dunder():
-    return f"The value of __name__ is: {__name__}"
+# @app.route('/name')  # by default, this is a GET request
+# def what_is_dunder():
+#     return f"The value of __name__ is: {__name__}"
 
 # @app.route('/method', methods=["GET", "POST"])
 # def method():
