cd97a367228af0e5232d9185d5bdc870dd31751a
bhoeft <brandonmhoeft@gmail.com>
2022-10-15 23:41:15 -0500

    add HTML jinja2 templates

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # runbook: $ `flask run` from CLI for dev/test
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')  # binds a URL to a function. By default these are GET requests
-def welcome():
-    return "My first Flask app! THIS IS ROOT!"
+def rootpage():
+    return render_template("index.html")  # looks in templates/
 
 @app.route('/name')
 def what_is_dunder():
