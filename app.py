ec752ffd943ffc8df6be4a34af8af9cb6c360705
bhoeft <brandonmhoeft@gmail.com>
2022-10-16 18:20:46 -0500

    add: extend the form to collect favorite food and print it back

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])  # app.route binds a URL to a function
 def rootpage():
     name = ''
+    food = ''
     if request.method == "POST" and "username" in request.form:
         name = request.form.get("username")
+        food = request.form.get("fave_food")
     return render_template("index.html",  # looks in templates/
-                           name_entered=name)  # we're giving name variable to our
+                           name_entered=name,
+                           food_entered=food)  # we're giving name variable to our
 
 # @app.route('/name')  # by default, this is a GET request
 # def what_is_dunder():
