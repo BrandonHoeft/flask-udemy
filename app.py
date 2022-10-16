9f9ffeaa6bc7041302820e7318cb7888183c0b65
bhoeft <brandonmhoeft@gmail.com>
2022-10-16 17:59:43 -0500

    upd: variable name -> name_entered

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def rootpage():
     if request.method == "POST" and "username" in request.form:
         name = request.form.get("username")
     return render_template("index.html",  # looks in templates/
-                           name=name)  # we're giving name variable to our
+                           name_entered=name)  # we're giving name variable to our
 
 # @app.route('/name')  # by default, this is a GET request
 # def what_is_dunder():
