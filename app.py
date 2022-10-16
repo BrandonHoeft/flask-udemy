6d9941211aed209413f18e5f4b4e5de42bac01f9
bhoeft <brandonmhoeft@gmail.com>
2022-10-16 14:24:15 -0500

    add: a button that takes user input and renders it to the root page

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')  # binds a URL to a function. By default these are GET requests
+@app.route('/', methods=["GET","POST"])  # app.route binds a URL to a function
 def rootpage():
-    return render_template("index.html")  # looks in templates/
+    name = ''
+    if request.method == "POST" and "username" in request.form:
+        name = request.form.get("username")
+        return render_template("index.html",  # looks in templates/
+                               name=name)  # we're giving name variable to our
 
-@app.route('/name')
+@app.route('/name')  # by default, this is a GET request
 def what_is_dunder():
     return f"The value of __name__ is: {__name__}"
 
-@app.route('/method', methods=["GET", "POST"])
-def method():
-    if request.method == "POST":
-        return "You've used a POST request"
-    else:
-        return "You're probably using a GET request, as it's a default!"
+# @app.route('/method', methods=["GET", "POST"])
+# def method():
+#     if request.method == "POST":
+#         return "You've used a POST request"
+#     else:
+#         return "You're probably using a GET request, as it's a default!"
